acb5fcc62c7ae00e01923dfd1d64e8c5fc7bb8ad
navyney <boontharika2548@gmail.com>
2024-08-06 20:41:20 +0700

    hdhdhsd

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,38 @@
 "use client";
 
+import UserCard from "@/components/UserCard";
 import axios from "axios";
 import { useState } from "react";
+import { UserCardProps } from "@/libs/types";
+import { useEffect } from "react";
+import { cleanUser } from "@/libs/cleanUser";
+
+
 
 export default function RandomUserPage() {
   // annotate type for users state variable
-  const [users, setUsers] = useState(null);
-
+  const [users, setUsers] = useState<UserCardProps[]>([]);
   const [isLoading, setIsLoading] = useState(false);
   const [genAmount, setGenAmount] = useState(1);
+  const [isFirst,setisFirst] =useState(true)
+  useEffect(() => {
+    if(isFirst) {
+        setisFirst(false);
+        return;
+      }
+    const jsonStr = JSON.stringify(genAmount);
+    localStorage.setItem('genAmount', jsonStr);
+  },[genAmount]);
+
+  useEffect(() => {
+    const jsonStr = localStorage.getItem('genAmount');
+    if(jsonStr !== null){
+      const newGenAmount = JSON.parse(jsonStr);
+      setGenAmount(newGenAmount);
+    }
+  },[])
+
+
 
   const generateBtnOnClick = async () => {
     setIsLoading(true);
@@ -17,7 +41,8 @@ export default function RandomUserPage() {
     );
     setIsLoading(false);
     const users = resp.data.results;
-
+    const cleanedUsers = users.map(cleanUser);
+    setUsers(cleanedUsers)
     //Your code here
     //Process result from api response with map function. Tips use function from /src/libs/cleanUser
     //Then update state with function : setUsers(...)
@@ -32,7 +57,7 @@ export default function RandomUserPage() {
           className="form-control text-center"
           style={{ maxWidth: "100px" }}
           type="number"
-          onChange={(e) => setGenAmount(e.target.value)}
+          onChange={(e) => setGenAmount(+e.target.value)}
           value={genAmount}
         />
         <button className="btn btn-dark" onClick={generateBtnOnClick}>
@@ -42,7 +67,7 @@ export default function RandomUserPage() {
       {isLoading && (
         <p className="display-6 text-center fst-italic my-4">Loading ...</p>
       )}
-      {users && !isLoading && users.map(/*code map rendering UserCard here */)}
+      {users && !isLoading && users.map((user)=>(<UserCard key = {user.email} {...user}/>))}
     </div>
   );
 }
